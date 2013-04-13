4122b20069fb5ff15b4187f93b2db1b23fa26740
Daniel Hensby <dan@hens.by>
2013-04-13 14:09:04 +0100

    Better Brief skeleton additions Changes to the installer to take account of our own skeleton changes Including - Removing tutorial theme - we add our own at run time - Adding our logo to assets - Adding our favicon - Adding our Google Webmasters ID file - Adding a robots.txt file - Adding our skeleton to the code folder - Adding our JS file for excluding admins - Default config options - Edits to Page.php - Adding HomePage Page type - .htaccess modifications to force www and other improvements

---
+++ mysite/code/Pages/HomePage.php
@@ -0,0 +1,38 @@
+<?php
+
+class HomePage extends Page {
+
+	// tree customisation
+	//static $icon = "";
+	//static $allowed_children = array("SiteTree"); // set to string "none" or array of classname(s)
+	//static $default_child = "Page"; //one classname
+	//static $default_parent = null; // NOTE: has to be a URL segment NOT a class name
+	//static $can_be_root = true; //
+	//static $hide_ancestor = null; //dont show ancestry class
+
+	public static
+		$db = array(
+		);
+
+	public function canCreate($member = null) {
+		return DataObject::get_one('HomePage') ? false : parent::canCreate($member);
+	}
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		//don't let them change the home page URL... it's a pain when they do
+		$fields->removeFieldFromTab('Root.Content.Metadata','URL');
+
+		return $fields;
+	}
+
+}
+
+class HomePage_Controller extends Page_Controller {
+
+	public static
+		$allowed_actions = array(
+		);
+
+}
