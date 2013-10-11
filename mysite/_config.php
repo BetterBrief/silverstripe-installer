c5dbe1a6ecab963f285d5126406c1d251af02029
Daniel Hensby <dan@hens.by>
2013-10-11 16:52:02 +0100

    Moving config options to YML

--- mysite/_config.php
+++ mysite/_config.php
@@ -29,44 +29,11 @@ if (Director::isLive()) {
 	//Config::inst()->update('Email', 'admin_email', '');
 }
 
-MySQLDatabase::set_connection_charset('utf8');
-
-DataObject::add_extension('Image', 'BBImageDecorator'); //enhancing the image class with a decorator
-DataObject::add_extension('SiteConfig','BBSiteConfigDecorator'); //enhancing the siteconfig with a decorator
-
-GD::set_default_quality(85);
-
-// Set the correct default language, this is used for Users
-// and for the content-language meta tag
-//i18n::set_default_lang('en_GB');
-//i18n::set_default_locale('en_GB');
-i18n::set_locale('en_GB');
-
-if (class_exists('DataObjectManager')){
-    DataObjectManager::allow_assets_override(true);
-}
-
 // stop the user being able to select h1 in the editor!
 HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats', 'p,h2,h3,h4,h5,h6,address,pre');
-
-//set admin user var for analytics for admin users
-LeftAndMain::require_javascript('mysite/javascript/admin-analytics.js');
 
 //allow full search of the site
 //FulltextSearchable::enable();
 
-// Breadcrumb delimiter
-//SiteTree::$breadcrumbs_delimiter = " - ";
-
-//stop default pages
-if(class_exists('SiteTree')) {
-	SiteTree::enable_nested_urls();
-	SiteTree::set_create_default_pages(false);
-}
-
 //removes m parameter
 Requirements::set_suffix_requirements(false);
-
-// This line set's the current theme. More themes can be
-// downloaded from http://www.silverstripe.org/themes/
-SSViewer::set_theme('default');
